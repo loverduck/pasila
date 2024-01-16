552d6fd39ecbbd2dc94efb4ba45e26f5ebfe17fa
전경향 <wjsrudgid@naver.com>
2024-01-16 17:26:49 +0900

    feat: [#S10P12A402-67] 메세지 기반 채팅 DAO

--- backend/src/main/java/com/ssafy/WebRTC/Live/DAO/ChatDAO.java
+++ backend/src/main/java/com/ssafy/WebRTC/Live/DAO/ChatDAO.java
@@ -19,7 +19,7 @@ public class ChatDAO {
     }
 
     // 실시간 라이브 조회
-    public List<ChatRoomDTO> selectAllLive() {
+    public List<ChatRoomDTO> getLiveList() {
         List<ChatRoomDTO> chatRooms = new ArrayList<>(chatRoomDTOMap.values());
         Collections.reverse(chatRooms);
 
@@ -31,15 +31,50 @@ public class ChatDAO {
         return chatRoomDTOMap.get(roomId);
     }
 
-    // title 기준 채팅방 검색
-
     // 채팅방 생성
     public ChatRoomDTO createChatRoom(String title){
+        // 채팅방 생성
+        ChatRoomDTO chatRoom = new ChatRoomDTO().create(title);
 
+        chatRoomDTOMap.put(chatRoom.getRoomId(), chatRoom);
+        return chatRoom;
     }
 
-    // 채팅방 입장 - 인원 + 1
+    // 채팅방 입장: 인원 + 1
+    public void increaseUserCnt(String roomId) {
+        ChatRoomDTO room = chatRoomDTOMap.get(roomId);
+        room.setUserCount(room.getUserCount() + 1);
+    }
 
-    // 채팅방 퇴장 - 인원 - 1
+    // 채팅방 퇴장: 인원 - 1
+    public void decreaseUserCnt(String roomId){
+        ChatRoomDTO room = chatRoomDTOMap.get(roomId);
+        room.setUserCount(room.getUserCount() - 1);
+    }
 
+    // 시청자 명단에서 사용자 추가
+    public String enterUser(String roomId, String userId, String userChannel){
+        ChatRoomDTO room = chatRoomDTOMap.get(roomId);
+        room.getUserList().put(userId, userChannel);
+
+        return userId;
+    }
+
+    // 시청자 명단에서 사용자 삭제
+    public String exitUser(String roomId, String userId){
+        ChatRoomDTO room = chatRoomDTOMap.get(roomId);
+        room.getUserList().remove(userId);
+
+        return userId;
+    }
+
+    // 시청자 명단 조회
+    public ArrayList<String> getUserList(String roomId){
+        ArrayList<String> userList = new ArrayList<>();
+
+        ChatRoomDTO room = chatRoomDTOMap.get(roomId);
+        room.getUserList().forEach((k, v) -> userList.add(v));
+
+        return userList;
+    }
 }
